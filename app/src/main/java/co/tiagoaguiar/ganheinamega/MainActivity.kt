35244527fb849749f8c79aca0990adff439e8199
amanda cleto <amanda.cleto.dev@gmail.com>
2023-05-02 14:51:22 -0300

    feat: create the mega sena logic and view

---
+++ app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
@@ -0,0 +1,84 @@
+package co.tiagoaguiar.ganheinamega
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
+
+/* Inherits AppCompatActivity to turn this MainActivity as one Activity */
+/* This MainActivity is the first activity of the app, because it is set on AndroidManifest.xml,
+   [you can change it]. */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val editText: EditText = findViewById(R.id.edit_number)
+        val txtResult: TextView = findViewById(R.id.txt_result)
+        val btnGenerate: Button = findViewById(R.id.btn_generate)
+
+        //How to listen events
+        //option 1: by using XML - add onClick at the button that is filled with the name of a function
+        //option 2: variable needs to be of type "View.OnClickListener (interface)"; to do that, create
+        //          an object variable that implements View.OnClickListener
+//        btnGenerate.setOnClickListener(buttonClickedListener)
+
+        //option 3: easiest one, create a block of code that will be shot by setOnClickListener
+        btnGenerate.setOnClickListener {
+            val text = editText.text.toString()
+            numberGenerator(text, txtResult)
+        }
+    }
+
+    private fun numberGenerator(text: String, txtResult: TextView) {
+        if (text.isEmpty()) {
+            Toast.makeText(this, "Informe um número entre 6 e 15", Toast.LENGTH_LONG).show()
+            txtResult.text = ""
+            return
+        }
+
+        val amount = text.toInt()
+        if (amount < 6 || amount > 15) {
+            Toast.makeText(this, "Informe um número entre 6 e 15", Toast.LENGTH_LONG).show()
+            txtResult.text = ""
+            return
+        }
+
+
+        var arrangeOfNumbers = mutableSetOf<Int>();
+        while (true) {
+            if (arrangeOfNumbers.size == amount) {
+                break
+            } else {
+                val random = Random.nextInt(60) //0..59
+                arrangeOfNumbers.add(random + 1) //if 0 -> 1 | if 59 -> 60
+                continue
+            }
+        }
+        txtResult.text = arrangeOfNumbers.joinToString(" - ")
+    }
+
+
+    //option 2:
+    // version Lambda
+//    val buttonClickedListener = View.OnClickListener { Log.i("Botão", "clicou nele opção 2") }
+
+    // version larger of the above Lambda
+//    val buttonClickedListener = object : View.OnClickListener {
+//        //sdk android calls this onClick after the button is clicked
+//        override fun onClick(v: View?) {
+//            Log.i("Botão", "clicou nele opção 2")
+//        }
+//    }
+
+    //option 1: Function listened by the button
+//    fun generateNumbers(view: View) {
+//        Log.i("Botão", "clicou nele opção 1")
+//    }
+
+}
